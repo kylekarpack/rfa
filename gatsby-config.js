272ad9678749d5b67d0bd460dae935ab6ea3f46e
Kyle Karpack <karpack@adobe.com>
2020-04-18 10:26:36 -0700

    Metadata changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Rwanda Faith Academy',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Main site for the Rwanda Faith Academy operation in the United States',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
