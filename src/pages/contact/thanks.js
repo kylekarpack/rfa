d9c12f2af5d8facec3399dd1538d5f2924fcb75d
Kyle Karpack <karpack@adobe.com>
2020-04-19 09:58:41 -0700

    Contact thanks fixed

--- src/pages/contact/thanks.js
+++ src/pages/contact/thanks.js
@@ -3,11 +3,19 @@ import Layout from '../../components/Layout'
 
 export default () => (
   <Layout>
+    <div className="title-area">
+      <div className="section">
+        <div className="container">
+          <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
+            Thank You!
+          </h2>
+        </div>
+      </div>
+    </div>
     <section className="section">
       <div className="container">
         <div className="content">
-          <h1>Thank you!</h1>
-          <p>This is a custom thank you page for form submissions</p>
+          <p>We will get back to you shortly</p>
         </div>
       </div>
     </section>
