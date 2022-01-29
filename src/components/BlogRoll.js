956c98b04d7e43518ed6de745cd9daf018e7c1d5
Kyle Karpack <kylekarpack@gmail.com>
2022-01-29 11:46:15 -0800

    Update image links on blog

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Link, graphql, StaticQuery } from 'gatsby'
-import PreviewCompatibleImage from './PreviewCompatibleImage'
-import { Heading } from './Heading'
+import React from "react";
+import PropTypes from "prop-types";
+import { Link, graphql, StaticQuery } from "gatsby";
+import PreviewCompatibleImage from "./PreviewCompatibleImage";
+import { Heading } from "./Heading";
 
 class BlogRoll extends React.Component {
   render() {
-    const { data } = this.props
-    const { edges: posts } = data.allMarkdownRemark
+    const { data } = this.props;
+    const { edges: posts } = data.allMarkdownRemark;
 
     return (
       <div>
@@ -16,16 +16,13 @@ class BlogRoll extends React.Component {
             <div className="is-parent" key={post.id}>
               <article
                 className={`blog-list-item box ${
-                  post.frontmatter.featuredpost ? 'is-featured' : ''
-                  }`}
-              >
+                  post.frontmatter.featuredpost ? "is-featured" : ""
+                }`}>
                 <header className="is-full">
-
                   <p className="post-meta">
                     <Link
                       className="title has-text-primary is-size-4"
-                      to={post.fields.slug}
-                    >
+                      to={post.fields.slug}>
                       <Heading title={post.frontmatter.title} />
                     </Link>
                     <br />
@@ -38,12 +35,14 @@ class BlogRoll extends React.Component {
                   <div className="columns">
                     {post.frontmatter.featuredimage ? (
                       <div className="featured-thumbnail column is-one-quarter">
-                        <PreviewCompatibleImage
-                          imageInfo={{
-                            image: post.frontmatter.featuredimage,
-                            alt: `featured image thumbnail for post ${post.frontmatter.title}`,
-                          }}
-                        />
+                        <Link to={post.fields.slug}>
+                          <PreviewCompatibleImage
+                            imageInfo={{
+                              image: post.frontmatter.featuredimage,
+                              alt: `featured image thumbnail for post ${post.frontmatter.title}`,
+                            }}
+                          />
+                        </Link>
                       </div>
                     ) : null}
                     <div className="column is-three-quarters">
@@ -59,7 +58,7 @@ class BlogRoll extends React.Component {
             </div>
           ))}
       </div>
-    )
+    );
   }
 }
 
@@ -69,43 +68,44 @@ BlogRoll.propTypes = {
       edges: PropTypes.array,
     }),
   }),
-}
+};
 
 export default () => (
   <StaticQuery
-    query={graphql`query BlogRollQuery {
-  allMarkdownRemark(
-    sort: {order: DESC, fields: [frontmatter___date]}
-    filter: {frontmatter: {templateKey: {eq: "blog-post"}}}
-  ) {
-    edges {
-      node {
-        excerpt(pruneLength: 400)
-        id
-        fields {
-          slug
-        }
-        frontmatter {
-          title
-          templateKey
-          date(formatString: "MMMM DD, YYYY")
-          featuredpost
-          featuredimage {
-            childImageSharp {
-              gatsbyImageData(
-                width: 300
-                quality: 50
-                placeholder: TRACED_SVG
-                layout: CONSTRAINED
-              )
+    query={graphql`
+      query BlogRollQuery {
+        allMarkdownRemark(
+          sort: { order: DESC, fields: [frontmatter___date] }
+          filter: { frontmatter: { templateKey: { eq: "blog-post" } } }
+        ) {
+          edges {
+            node {
+              excerpt(pruneLength: 400)
+              id
+              fields {
+                slug
+              }
+              frontmatter {
+                title
+                templateKey
+                date(formatString: "MMMM DD, YYYY")
+                featuredpost
+                featuredimage {
+                  childImageSharp {
+                    gatsbyImageData(
+                      width: 300
+                      quality: 50
+                      placeholder: TRACED_SVG
+                      layout: CONSTRAINED
+                    )
+                  }
+                }
+              }
             }
           }
         }
       }
-    }
-  }
-}
-`}
+    `}
     render={(data, count) => <BlogRoll data={data} count={count} />}
   />
-)
+);
